2fe6160f9229e37a8ed14bd4e995d438dca21c6c
Eli <elihanover@yahoo.com>
2018-02-13 19:48:09 -0500

    Refactored to eliminate extraneous functions

--- DLottery.sol
+++ DLottery.sol
@@ -3,37 +3,66 @@ pragma solidity 0.4.19;
 
 contract DLottery {
 
-    // do I want this public or private?
-    // index = ticket number,
-    // value = address
+    // current lotto info
     address[] public owners;
-
-
-    uint[3] public lastLottoInfo; // winner, pot_size, duration
-
-    // lotto info
     uint public lotteryEndTime;
     uint public duration;
-    uint public potSize;
     bool public ended;
+    address public winner;
 
-    address winner public ;
+    // last lotto
+    uint[3] public lastLottoInfo; // winner, pot_size, duration
 
+    /*
+      Intra-Contract Functions
+    */
+    // Contract constructor, called when the contract is deployed
+    // When launched, make auctions one day long
     function DLottery() public {
-        commenceLottery();
+        commenceLottery(86400); // 86400 seconds in one day
     }
 
-    function commenceLottery(uint durationSeconds) public {
-        // Start lottery in constructor and when each lottery ends
-        duration = durationSeconds;
-        lotteryEndTime = now + durationSeconds; // set endtime to now + time
+    // Start lottery in constructor and when each lottery ends
+    function commenceLottery(uint durationMinutes) public {
+        // reset params
+        winner = ; // reset winner to what?
+        duration = durationMinutes;
+        lotteryEndTime = now + durationMinutes*60; // set endtime to now + time
         owners = address[]; // reset owners
     }
 
-    function chooseWinner() public returns (address) {
-        // pick random ticket and return the address who owns it
-        uint windex = 1;// randint(0, ticketOwners.keys.size-1)
+    // Todo //
+    // Called when the lottery end event detected
+    function closeLottery() public {
+        // conditions
+        require(now >= lotteryEndTime); // ensure no early call
+        require(!ended); // make sure not already called
+
+        ended = true;
+
+        // choose winner
+        uint windex = randint(0, owners.length-1); // PICK RANDOM WINNER TODO
         winner = owners[windex];
+        winner.transfer(owners.length); // send ether to the winner
+
+        // update info because lotto closed
+        lastLottoInfo = [winner, owners.length, duration];
+
+        // start new lotto
+        commenceLottery(duration);
+    }
+
+    /*
+      Interface Functions:
+    */
+    function getLastLottoData() public returns (uint[3]) {
+        // winner, amount, duration
+        return lastLottoInfo;
+    }
+
+    function getLottoData() public returns (uint[3]) {
+        // return data about this lotto
+        return [duration, lotteryEndTime, owners.length];
     }
 
     // 1 ticket = 1 wei
@@ -47,46 +76,6 @@ contract DLottery {
         for (uint i = 0; i < tickets; i++) {
             owners.push(msg.sender);
         }
-    }
-
-    function closeLottery() public {
-        // conditions
-        require(now >= lotteryEndTime); // ensure no early call
-        require(!ended); // make sure not already called
-
-        ended = true;
-
-        // set winner so that winner can claim reward
-        chooseWinner();
-
-        // don't want to just send ether because it could invoke an untrusted contract
-        // instead you want to publish the winner and allow them to withdraw funds
-
-    }
-
-    function getWinner() public returns (address) {
-        // called by dapp in order to publish winner
-        if (winner != None) {
-            // update lastlottoinfo with closed lotto info
-            lastLottoInfo = [winner, pot_size, duration];
-
-            // reset params
-            winner = None;
-            pot_size = 0;
-            duration = duration; // update with voting later
-
-            return winner;
-        }
-    }
-
-    function getLastLottoData() public returns (uint[3]) {
-        // winner, amount, duration
-        return lastLottoInfo;
-    }
-
-    function getLottoData() public returns (uint[3]) {
-        // return data about this lotto
-        return [duration, lotteryEndTime, potSize];
     }
 
 
