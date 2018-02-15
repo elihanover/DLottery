ac24c4cd464a50b67ddae750021008aec8238acd
Eli <elihanover@yahoo.com>
2018-02-14 21:59:30 -0500

    Sol ready to test
    Calls go through, but fails on testnet…

--- DLottery.sol
+++ DLottery.sol
@@ -6,7 +6,7 @@ contract DLottery {
     // current lotto info
     address[] private empty;
     address[] private owners;
-    uint private lotteryEndTime;
+    uint private lotteryEndBlock;
     uint private duration;
     bool private ended; // what would I need an ended for?  guessing i'm gonna need it
 
@@ -24,32 +24,43 @@ contract DLottery {
         lastWinner = 1;
         lastPot = 1;
         lastDuration = 1;
-        commenceLottery(10); // 86400 seconds in one day
+        commenceLottery(1); // 86400 seconds in one day
     }
 
     /*
       Interface Functions:
     */
     function getLastWinner() public returns (address) {
+        checkClosed();
         return lastWinner;
     }
 
     function getLastPot() public returns (uint) {
+        checkClosed();
         return lastPot;
     }
 
     function getLastDuration() public returns (uint) {
+        checkClosed();
         return lastDuration;
     }
 
     function getLottoData() public returns (uint[3]) {
+        checkClosed();
         // return data about this lotto
-        return [duration, lotteryEndTime, owners.length];
+        return [duration, lotteryEndBlock, owners.length];
+    }
+
+    // Called by dapp to check if the auction is over, and
+    //  triggers closeLottery if so
+    function getBlocksRemaining() public returns (uint) {
+        checkClosed();
+        return lotteryEndBlock - block.number;
     }
 
     // 1 ticket = 1 wei
     function buyTickets(uint tickets) public payable returns (uint) {
-
+        checkClosed();
         //uint tickets = msg.value; // get wei sent
 
         // append users address tickets times
@@ -59,25 +70,15 @@ contract DLottery {
         return owners.length;
     }
 
-    // Called by dapp to check if the auction is over, and
-    //  triggers closeLottery if so
-    function getTimeRemaining() public returns (uint) {
-        if (now >= lotteryEndTime) {
-            test = 2;
-            closeLottery();
-            test = 3;
-        }
-        test = 4;
-        return lotteryEndTime - now;
-    }
-
-    // Todo //
     // Called when the lottery end event detected
-    function closeLottery() private {
-        // choose winner
-        uint windex = uint(block.blockhash(block.number-1)) % owners.length; // PICK RANDOM WINNER TODO
-        address winner = owners[windex];
-        winner.transfer(owners.length); // send ether to the winner
+    function closeLottery() public {
+
+        // if participants, get winner and send winnings
+        if (owners.length > 0) {
+            uint windex = uint(block.blockhash(block.number-1)) % owners.length;
+            address winner = owners[windex];
+            winner.send(lastPot); // send ether to winner
+        }
 
         // update info because lotto closed
         lastWinner = winner;
@@ -88,11 +89,18 @@ contract DLottery {
         commenceLottery(duration);
     }
 
+    function checkClosed() private {
+        test = 3;
+        if (block.number >= lotteryEndBlock) {
+            //closeLottery();
+        }
+    }
+
     // Start lottery in constructor and when each lottery ends
-    function commenceLottery(uint durationSeconds) private {
+    function commenceLottery(uint durationBlocks) private {
         // reset params
-        duration = durationSeconds;
-        lotteryEndTime = now + durationSeconds; // set endtime to now + time
+        duration = durationBlocks;
+        lotteryEndBlock = block.number + duration;
         owners = empty; // reset owners
     }
 }
